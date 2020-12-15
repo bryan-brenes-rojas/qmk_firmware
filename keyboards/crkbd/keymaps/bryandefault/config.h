da503c63fd9351da929b7caabd762ca3ffca409d
Bryan Brenes <bryanbrenes1@gmail.com>
2020-12-14 23:19:46 -0600

    tapping term reduction

--- keyboards/crkbd/keymaps/bryandefault/config.h
+++ keyboards/crkbd/keymaps/bryandefault/config.h
@@ -33,7 +33,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define TAPPING_FORCE_HOLD
 #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_TERM 450
+#define TAPPING_TERM 200 // 450
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
