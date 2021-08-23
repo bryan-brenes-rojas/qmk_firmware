93b81f801296e6da3286383d5c02dbdd4616b625
Bryan Brenes <bryanbrenes1@gmail.com>
2021-08-22 20:17:20 -0600

    Remove modifiers from alphas

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bryan/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bryan/keymap.c
@@ -23,13 +23,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT_5x6(
      KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  , KC_BSPC,
-     KC_ESC , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  , KC_MINS,
-     KC_CAPS, LCA_T(KC_A)  , LCTL_T(KC_S)  , RALT_T(KC_D)  , KC_F  , KC_G  ,  KC_H  , KC_J  , RALT_T(KC_K)  , RCTL_T(KC_L)  ,LCA_T(KC_SCLN), KC_QUOT,
-     KC_LSFT, LGUI_T(KC_Z)  , KC_X  , LALT_T(KC_C)  , KC_V  , KC_B  ,         KC_N  , KC_M  ,KC_COMM,KC_DOT ,RGUI_T(KC_SLSH), KC_RSFT,
+     RALT_T(KC_ESC) , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,                         KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  , RALT_T(KC_MINS),
+     LCTL_T(KC_CAPS), KC_A  ,  KC_S  , KC_D  , KC_F  , KC_G  ,  KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN, RCTL_T(KC_QUOT),
+     KC_LSFT, LGUI_T(KC_Z)  , KC_X  , KC_C  , KC_V  , KC_B  ,         KC_N  , KC_M  ,KC_COMM,KC_DOT ,RGUI_T(KC_SLSH), KC_RSFT,
                       KC_LEFT,KC_RIGHT,                                       KC_DOWN, KC_UP,
                                       RAISE,_______,                           _______, LOWER,
-                                      KC_LALT, LT(2,KC_BSPC),                 LT(1,KC_SPC), KC_RCTL,
-                                      KC_LCTL, KC_TAB,                        KC_ENT, KC_RALT
+                                      KC_LALT, LT(2,KC_BSPC),                 LT(1,KC_SPC), KC_RALT,
+                                      KC_LCTL, LCA_T(KC_TAB),                 LCA_T(KC_ENT), KC_RCTL
   ),
 
   [_LOWER] = LAYOUT_5x6(
